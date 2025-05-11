772301ce2d29708e6bda67b92f9b2786c216d9b1
ahmadounaa <amelless1230@gmail.com>
2025-05-11 18:21:06 +0000

    Amadou cissé

---
+++ jeux.py
@@ -0,0 +1,10 @@
+import random 
+x = random.randint(0,100)
+joueur = int(input("Entrez un nombre de votre choix : "))
+while joueur != x
+if joueur == x:
+    print("Félicitation")
+    elif joueur < x :
+        print("trop grand")
+        else:
+            print("trop petit")
